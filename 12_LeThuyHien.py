75e05be2121ffab2282e946f23f138528b299a25
lehien16 <lehien161205@gmail.com>
2025-10-20 18:59:07 +0700

    feat: Implement print list function

--- 12_LeThuyHien.py
+++ 12_LeThuyHien.py
@@ -29,7 +29,12 @@ def print_student_list():
       trên một dòng theo định dạng: " - Ten: [Họ tên], Nam sinh: [Năm sinh], Dia chi: [Địa chỉ]"
     """
     ### VIẾT CODE CỦA BẠN VÀO ĐÂY ###
-    pass # Xóa dòng này khi bạn viết code
+    print("--- DANH SACH SINH VIEN ---")
+    if not student_list:
+        print("Danh sach trong.")
+    else:
+        for s in student_list:
+            print(f" - Ten: {s['name']}, Nam sinh: {s['year_of_birth']}, Dia chi: {s['address']}")
 
 def search_student(search_name):
     """
